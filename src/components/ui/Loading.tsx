626633a2f4c52d2233285d1a6431d99556d3f274
Stars1104 <cooljob.love1104@gmail.com>
2025-03-28 02:20:02 -0700

    updated loading component

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center h-screen">
             <div className="w-16 h-16 border-8 border-dashed rounded-full animate-spin border-blue-600"></div>
             <span>Loading</span>
         </div>
